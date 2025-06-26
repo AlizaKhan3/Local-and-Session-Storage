84bd34effac124817713f2d0fd688954763a3b4d
Aliza Khan <alizaadi2@gmail.com>
2025-06-26 20:41:22 +0500

    Using react hook form

--- src/components/React Hook Form/ReactHookForm.jsx
+++ src/components/React Hook Form/ReactHookForm.jsx
@@ -5,14 +5,37 @@ const ReactHookForm = () => {
 
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
-
+    console.log(watch("email"));
     return (
-        <div>
+        <div className="flex flex-col gap-3">
             <h1>React hook form</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <InputComponent placeholder="Enter Your Name" {...register("name")}/>
-                <InputComponent placeholder="Enter Email" {...register("")} />
-                <InputComponent placeholder="Enter Password" />
+                <InputComponent placeholder="Enter Your Name" {...register("name", { required: "true" })} />
+                {errors.name && <span>Name field is required</span>}
+                <InputComponent placeholder="Enter Email" {...register("email", { required: "true" })} />
+                {errors.email && <span>Email field is required</span>}
+                <InputComponent placeholder="Enter Password" {...register("password", { required: "true" })} />
+                {errors.password && <span>Password field is required</span>}
+                <hr />
+                <input
+                    {...register("test", {
+                        maxLength: {
+                            value: 2,
+                            message: <p>max length must be 2</p> // JS only: <p>error message</p> TS only support string
+                        }
+                    })}
+                />
+                <hr />
+                <select {...register("gender", { required: "true" })} className="block">
+                    <option value="">Select Gender</option>
+                    <option value="female">female</option>
+                    <option value="male">male</option>
+                    <option value="other">other</option>
+                </select>
+
+                <input type="submit" />
+                {/* || errors.email || errors.name */}
+
             </form>
 
         </div>
