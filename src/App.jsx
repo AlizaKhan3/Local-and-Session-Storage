afcfb4a19835edbcc9a9ff666b603cbb2a68963e
Aliza Khan <alizaadi2@gmail.com>
2025-06-26 21:25:13 +0500

    Set react hook form data in Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import AppLocalStorage from './components/LocalStorage/LocalStorage';
 import ReactHookForm from './components/React Hook Form/ReactHookForm';
 import AppSessionStorage from './components/SessionStorage/SessionStorage';
+import HandleData from './components/React Hook Form/handleLocalStorage';
 
 function App() {
   return (
     <>
       {/* <AppLocalStorage /> */}
       {/* <AppSessionStorage/> */}
-      <ReactHookForm/>
+      {/* <ReactHookForm/> */}
+      <HandleData/>
     </>
   )
 }
--- src/components/React Hook Form/ReactHookForm.jsx
+++ src/components/React Hook Form/ReactHookForm.jsx
@@ -1,31 +1,57 @@
 import InputComponent from "../InputComponent"
 import { useForm } from "react-hook-form"
 
-const ReactHookForm = () => {
+// const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/
 
+const ReactHookFormApp = ({sendFormData}) => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-    console.log(watch("email"));
+    const onSubmit = data => {
+      const formData =  data;
+      console.log(data)
+      sendFormData(data)
+    }
+    // console.log(watch("email"));
+    
     return (
         <div className="flex flex-col gap-3">
             <h1>React hook form</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <InputComponent placeholder="Enter Your Name" {...register("name", { required: "true" })} />
-                {errors.name && <span>Name field is required</span>}
-                <InputComponent placeholder="Enter Email" {...register("email", { required: "true" })} />
-                {errors.email && <span>Email field is required</span>}
-                <InputComponent placeholder="Enter Password" {...register("password", { required: "true" })} />
-                {errors.password && <span>Password field is required</span>}
+                <InputComponent type="text" placeholder="Enter Your Name" {...register("name", { required: "Please enter your name" })} />
+                {errors.name && <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>}
+                <InputComponent type="email" placeholder="Enter Email"
+                    {...register("email", {
+                        required: "Please enter email",
+                        pattern: {
+                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                            message: "Invalid Email Address"
+                        }
+                    })} />
+
+                {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>}
+
+                <InputComponent type="password" placeholder="Password"
+                    {...register("password", {
+                        required: "Password is required",
+                        pattern: {
+                            value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/,
+                            message: "Password must be 8-16 chars, include upper, lower, number, special char"
+                        }
+                    })} />
+                {/* {errors.password && <span>Password field is required</span>} */}
+                {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>}
+
                 <hr />
                 <input
                     {...register("test", {
                         maxLength: {
                             value: 2,
-                            message: <p>max length must be 2</p> // JS only: <p>error message</p> TS only support string
+                            message: "max length must be 2" // JS only: <p>error message</p> TS only support string
                         }
                     })}
                 />
+                {errors.test && <span>max length exceeding 2</span>}
                 <hr />
+
                 <select {...register("gender", { required: "true" })} className="block">
                     <option value="">Select Gender</option>
                     <option value="female">female</option>
@@ -33,13 +59,11 @@ const ReactHookForm = () => {
                     <option value="other">other</option>
                 </select>
 
-                <input type="submit" />
-                {/* || errors.email || errors.name */}
-
+                <button type="submit">Submit</button>
             </form>
 
         </div>
     )
 }
 
-export default ReactHookForm
+export default ReactHookFormApp
